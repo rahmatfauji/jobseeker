4aedf872e507a6180194cd8d77210ac5764f90f7
Rahmat Fauji <rahmatfauji@gmail.com>
2019-07-10 13:19:23 +0700

    commit first jobsearch app

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Role;
+use App\User;
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role_admin = Role::where('name','admin')->first();
+        $admin = new User();
+        $admin->name = 'Mimin Admin';
+        $admin->email = 'miminadmin@example.com';
+        $admin->password = bcrypt('secret');
+        $admin->save();
+        $admin->roles()->attach($role_admin);
+    }
+}
